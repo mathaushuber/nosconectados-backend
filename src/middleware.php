3fe13c2207a0670ff8395ca90f440f188205de84
Mathaus Huber <huber.mathaus@gmail.com>
2023-01-29 20:57:42 -0300

    alterando permissões das rotas

--- src/middleware.php
+++ src/middleware.php
@@ -6,7 +6,8 @@ $app->add(new Tuupola\Middleware\JwtAuthentication([
     "header" => "Authorization",
     "regexp" => "/(.*)/",
     "path" => "/api", /* or ["/api", "/admin"] */
-    "ignore" => ["/api/login", "/api/v1/sensores/lista", "/api/v1/usuarios/cadastro", "/api/v1/sensores/lista-geral"],
+    "ignore" => ["/api/login", "/api/v1/sensores/lista", "/api/v1/usuarios/cadastro", "/api/v1/sensores/lista-geral",
+                 "/api/v1/sensores/data/", "/api/v1/atribuicao/lista-geral/"],
     "secret" => $container->get('settings')['secretKey']
 ]));
 
