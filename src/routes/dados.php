87263cead234994be095c713263c97a8378241a3
HuberM1998 <huber.mathaus@gmail.com>
2023-05-08 20:14:26 -0300

    pegando últimos 8 dados

--- src/routes/dados.php
+++ src/routes/dados.php
@@ -10,30 +10,31 @@ $app->group('/api/v1', function(){
 	
 	// Lista dados
 	$this->get('/dados/lista/{id}', function($request, $response, $args){
-		$dados = Dado::get();
-        foreach ($dados as $key => $value){
-			if($args['id'] == $value->id){
-				$sensorData[] = array(
-					"id" => $value->id,
-					"readAt" => $value->readAt,
-					"temperatureSoil" => $value->temperatureSoil,
-					"temperatureAir" => $value->temperatureAir,
-					"luminosity" => $value->luminosity,
-					"pluviometer" => $value->pluviometer,
-					"ultraviolet" => $value->ultraviolet,
-					"temperatureCase" => $value->temperatureCase,
-					"rainIntensity" => $value->rainIntensity,
-					"windDirection" => $value->windDirection,
-					"windSpeed" => $value->windSpeed,
-					"gas" => $value->gas,
-					"humidityAirRelative" => $value->humidityAirRelative,
-					"altitude" => $value->altitude,
-					"pressure" => $value->pressure,
-				);
-			}
+		$dados = Dado::where('id', $args['id'])->orderBy('readAt', 'desc')->take(8)->get();
+	
+		$sensorData = array();
+		foreach ($dados as $key => $value){
+			$sensorData[] = array(
+				"id" => $value->id,
+				"readAt" => $value->readAt,
+				"temperatureSoil" => $value->temperatureSoil,
+				"temperatureAir" => $value->temperatureAir,
+				"luminosity" => $value->luminosity,
+				"pluviometer" => $value->pluviometer,
+				"ultraviolet" => $value->ultraviolet,
+				"temperatureCase" => $value->temperatureCase,
+				"rainIntensity" => $value->rainIntensity,
+				"windDirection" => $value->windDirection,
+				"windSpeed" => $value->windSpeed,
+				"gas" => $value->gas,
+				"humidityAirRelative" => $value->humidityAirRelative,
+				"altitude" => $value->altitude,
+				"pressure" => $value->pressure,
+			);
 		}
-
+	
 		return $response->withJson( $sensorData, 200 );
 	});
+	
 
 });
