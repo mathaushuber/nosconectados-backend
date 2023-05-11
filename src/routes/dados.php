1551145dad9f12031996e347aad7b0586e1784e6
Mathaus Huber <huber.mathaus@gmail.com>
2023-05-10 22:55:33 -0300

    Corrigindo bugs

--- src/routes/dados.php
+++ src/routes/dados.php
@@ -8,34 +8,33 @@ use \Firebase\JWT\JWT;
 // Rotas para dados
 $app->group('/api/v1', function(){
 	
-// Lista dados
-$this->get('/dados/lista/{id}', function($request, $response, $args){
-    $dados = Dado::where('id', $args['id'])->oldest()->take(8)->get();
-
-    $sensorData = array();
-    foreach ($dados as $key => $value){
-        $sensorData[] = array(
-            "id" => $value->id,
-            "readAt" => $value->readAt,
-            "temperatureSoil" => $value->temperatureSoil,
-            "temperatureAir" => $value->temperatureAir,
-            "luminosity" => $value->luminosity,
-            "pluviometer" => $value->pluviometer,
-            "ultraviolet" => $value->ultraviolet,
-            "temperatureCase" => $value->temperatureCase,
-            "rainIntensity" => $value->rainIntensity,
-            "windDirection" => $value->windDirection,
-            "windSpeed" => $value->windSpeed,
-            "gas" => $value->gas,
-            "humidityAirRelative" => $value->humidityAirRelative,
-            "altitude" => $value->altitude,
-            "pressure" => $value->pressure,
-        );
-    }
-
-    return $response->withJson( $sensorData, 200 );
-});
-
+	// Lista dados
+	$this->get('/dados/lista/{id}', function($request, $response, $args){
+		$dados = Dado::where('id', $args['id'])->orderBy('readAt', 'desc')->take(8)->get();
+	
+		$sensorData = array();
+		foreach ($dados as $key => $value){
+			$sensorData[] = array(
+				"id" => $value->id,
+				"readAt" => $value->readAt,
+				"temperatureSoil" => $value->temperatureSoil,
+				"temperatureAir" => $value->temperatureAir,
+				"luminosity" => $value->luminosity,
+				"pluviometer" => $value->pluviometer,
+				"ultraviolet" => $value->ultraviolet,
+				"temperatureCase" => $value->temperatureCase,
+				"rainIntensity" => $value->rainIntensity,
+				"windDirection" => $value->windDirection,
+				"windSpeed" => $value->windSpeed,
+				"gas" => $value->gas,
+				"humidityAirRelative" => $value->humidityAirRelative,
+				"altitude" => $value->altitude,
+				"pressure" => $value->pressure,
+			);
+		}
+	
+		return $response->withJson( $sensorData, 200 );
+	});
 
 		// Lista sensores
 		$this->get('/dados/lista-geral', function($request, $response){
